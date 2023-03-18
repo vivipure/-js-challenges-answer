76bc07ec1aacaffa68f0d0cb197239672e224d28
vivipure <vivipureheart@gmail.com>
2023-03-18 16:42:34 +0000

    Deploying to gh-pages from @ vivipure/bagu-of-fe@2a6b054bf30f9d15c3bbe516c5a9d981d31df9f9 🚀

---
+++ assets/javascript_index.md.019cb930.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./chunks/framework.b74c2b66.js";const l=JSON.parse('{"title":"JavaScript","description":"","frontmatter":{"title":"JavaScript","lang":"zh-CN"},"headers":[],"relativePath":"javascript/index.md"}'),r={name:"javascript/index.md"};function c(n,s,i,o,p,d){return a(),e("div")}const m=t(r,[["render",c]]);export{l as __pageData,m as default};
